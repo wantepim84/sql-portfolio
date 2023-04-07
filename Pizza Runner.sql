769265f2d558a0c3077f88ee50e916dd18b39401
wantepim84 <125708034+wantepim84@users.noreply.github.com>
2023-04-07 21:28:03 +0100

    Update Pizza Runner.sql

--- Pizza Runner.sql
+++ Pizza Runner.sql
@@ -43,3 +43,39 @@ ALTER TABLE "customer_orders" ADD FOREIGN KEY ("order_id") REFERENCES "runner_or
 ALTER TABLE "customer_orders" ADD FOREIGN KEY ("pizza_id") REFERENCES "pizza_names" ("pizza_id");
 
 ALTER TABLE "customer_orders" ADD FOREIGN KEY ("pizza_id") REFERENCES "pizza_recipes" ("pizza_id");
+
+How many pizzas were ordered?
+select count(pizza_id) as total
+from customer_orders;
+
+How many unique customer orders were made?
+SELECT Count(*) AS Unique_orders
+FROM (SELECT DISTINCT exclusions,extras FROM Customer_orders);
+
+How many successful orders were delivered by each runner?
+SELECT runner_id, Count(*) AS successful_orders
+FROM runner_orders
+where cancellation <> 'restaurant cancellation' or 'customer cancellation'
+group by runner_id 
+;
+
+How many of each type of pizza was delivered?
+SELECT pizza_id, Count(pizza_id) as Total_amount_ordered
+FROM customer_orders
+group by pizza_id 
+;
+How many Vegetarian and Meatlovers were ordered by each customer?
+SELECT customer_id, pizza_id,count(pizza_id) as total
+FROM customer_orders
+group by customer_id, pizza_id
+;
+What was the maximum number of pizzas delivered in a single order?
+select order_id, max(total)
+from (select order_id, count(pizza_id) as total 
+from customer_orders
+group by order_id)
+;
+For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+How many pizzas were delivered that had both exclusions and extras?
+What was the total volume of pizzas ordered for each hour of the day?
+What was the volume of orders for each day of the week?
